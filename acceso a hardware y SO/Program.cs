c0329ec21cd7a46c6e5cf12b7658ac43c4c69137
Camilotr13 <juanctr2002@gmail.com>
2024-10-23 16:36:09 -0500

    first commit

---
+++ acceso a hardware y SO/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Management;
+using System.Net.NetworkInformation;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace acceso_a_hardware_y_SO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ObtenerNumeroDeSerieDisco();
+            ObtenerCantidadDeDiscos();
+            ObtenerInventarioGeneral();
+            ObtenerMacAddress();
+            AccesoRegistroEjemplo();
+            ObtenerProcesosActivos();
+            Console.ReadLine();
+        }
+
+        static void ObtenerNumeroDeSerieDisco()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+                foreach (ManagementObject wmi_HD in searcher.Get())
+                {
+                    Console.WriteLine("Modelo de Disco: " + wmi_HD["Model"]);
+                    Console.WriteLine("Número de Serie: " + wmi_HD["SerialNumber"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener el número de serie: " + ex.Message);
+            }
+        }
+
+        static void ObtenerCantidadDeDiscos()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+                int count = 0;
+
+                foreach (ManagementObject wmi_HD in searcher.Get())
+                {
+                    count++;
+                }
+
+                Console.WriteLine("Cantidad de unidades de disco: " + count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener la cantidad de discos: " + ex.Message);
+            }
+        }
+
+        static void ObtenerInventarioGeneral()
+        {
+            try
+            {
+                ManagementObjectSearcher cpuSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+                foreach (ManagementObject cpu in cpuSearcher.Get())
+                {
+                    Console.WriteLine("Procesador: " + cpu["Name"]);
+                }
+
+                ManagementObjectSearcher ramSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+                foreach (ManagementObject ram in ramSearcher.Get())
+                {
+                    Console.WriteLine("RAM: " + ram["Capacity"] + " bytes");
+                }
+
+                ManagementObjectSearcher nicSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter");
+                foreach (ManagementObject nic in nicSearcher.Get())
+                {
+                    Console.WriteLine("NIC: " + nic["Name"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener el inventario: " + ex.Message);
+            }
+        }
+
+        static void ObtenerMacAddress()
+        {
+            try
+            {
+                var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+                foreach (var ni in networkInterfaces)
+                {
+                    Console.WriteLine("Nombre: " + ni.Name);
+                    Console.WriteLine("MAC Address: " + ni.GetPhysicalAddress());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener la MAC Address: " + ex.Message);
+            }
+        }
+
+        static void AccesoRegistroEjemplo()
+        {
+            try
+            {
+                string clave = @"SOFTWARE\MiAplicacion";
+                RegistryKey registryKey = Registry.CurrentUser.CreateSubKey(clave);
+                registryKey.SetValue("MiValor", "ValorEjemplo");
+                Console.WriteLine("Clave creada y valor establecido.");
+
+                Console.WriteLine("Valor leído: " + registryKey.GetValue("MiValor"));
+
+                registryKey.SetValue("MiValor", "NuevoValor");
+                Console.WriteLine("Valor modificado: " + registryKey.GetValue("MiValor"));
+
+                registryKey.DeleteValue("MiValor");
+                Console.WriteLine("Valor borrado.");
+
+                registryKey.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al acceder al registro: " + ex.Message);
+            }
+        }
+
+        static void ObtenerProcesosActivos()
+        {
+            try
+            {
+                Process[] procesos = Process.GetProcesses();
+                Console.WriteLine("Procesos activos:");
+                foreach (Process p in procesos)
+                {
+                    Console.WriteLine($"ID: {p.Id}, Nombre: {p.ProcessName}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener procesos activos: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ acceso a hardware y SO/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("acceso a hardware y SO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("acceso a hardware y SO")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("12f6890c-9a6b-4fa9-ac14-87ad4e5d879a")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
